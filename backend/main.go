bd85fd5f61b454a18a19a345b061a304d352644e
OGATA Hiromu <hiromukkun@gmail.com>
2024-10-05 23:54:45 +0900

    add:db接続（上手く行っていない）

--- backend/main.go
+++ backend/main.go
@@ -2,11 +2,26 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	"context"
+	"log"
+	"nextjs-go-practice/ent"
+	_ "github.com/lib/pq"
 )
 
 func main() {
 	//Ginフレームワークのデフォルトの設定を使用してルータを作成
 	router := gin.Default()
+
+	//PostgreSQLに接続
+	client, err := ent.Open("postgres", "host=db port=5432 user=postgres dbname=db password=password sslmode=disable")
+
+	if err != nil {
+		log.Fatalf("failed opening connection to postgres: %v", err)
+	}
+	//Run the auto migration tool.
+	if err := client.Schema.Create(context.Background()); err != nil {
+		log.Fatalf("failed creating schema resources: %v", err)
+	}
 	
 	// ルートハンドラの定義
 	router.GET("/hoge", func(c *gin.Context) {
